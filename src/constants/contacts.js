2c021692e64e1b751258c6221cf0b0305f2aa01e
Igor <80968651+webdevbiv@users.noreply.github.com>
2025-07-30 16:19:43 +0300

    FIX | path

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,7 @@
-export const PATH_DB = 'src/db/db.json';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const PATH_DB = path.resolve(__dirname, '../db/db.json');
